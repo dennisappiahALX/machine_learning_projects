61d0d835db6dbd43cf247adb6b40bbca1b6bcac5
dennisappiahALX <appiahdennis1998@gmail.com>
2022-06-11 06:02:04 +0000

    configuration file

---
+++ ames_housing_prices_prediction-KaggleHackathon/src/config.py
@@ -0,0 +1,8 @@
+TRAIN_DATA = '../data/train.csv'
+TEST_DATA = '../data/test.csv'
+TRAIN_PROCESSED_DATA = '../data/train_processed_data.csv'
+TEST_PROCESSED_DATA = '../data/test_processed_data.csv'
+MODELS_PATH = '../models/'
+MODEL = ['linearregression', 'randomforest', 'xgb']
+
+
